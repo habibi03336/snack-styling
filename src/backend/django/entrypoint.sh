6f6e87f1f13c766ff85340d1327ed391f1f9dd20
최민수 <ms.choe015@gmail.com>
2022-08-02 15:52:17 +0900

    chore: fix shell script error

--- src/backend/django/entrypoint.sh
+++ src/backend/django/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 #!/bin/bash
 
-
 echo "[RUN] running migrations."
-python manage.py makemigrations --merge
-python manage.py migrate --noinput
+python manage.py makemigrations
+python manage.py migrate
 
 echo "[RUN] running application."
 python manage.py runserver 0.0.0.0:8000
