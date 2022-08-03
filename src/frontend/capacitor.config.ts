87604ed119eabb5109b6f746b226e5ec94e77342
하지훈 <jnh03336@gmail.com>
2022-08-03 09:49:58 +0900

    chore: config files

---
+++ src/frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.ionic.starter",
+  appName: "coffee-styling",
+  webDir: "build",
+  bundledWebRuntime: false,
+};
+
+export default config;
