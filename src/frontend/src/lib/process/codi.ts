db21f1fd92238faf1e30d4559e59a82e9ec29788
하지훈 <jnh03336@gmail.com>
2022-08-05 12:33:50 +0900

    Feat: create modules

---
+++ src/frontend/src/lib/process/codi.ts
@@ -0,0 +1,35 @@
+import * as I from "../../interfaces";
+
+const categoryMap = new Map([
+  ["top", "상의"],
+  ["bottom", "하의"],
+]);
+
+export const makeCodiTemplate = (codis: I.Codi[], template: I.CodiTemplate) => {
+  const codiArray: I.CodiTemplate[] = [];
+  codis.forEach((codi: I.Codi) => {
+    const theCodi: I.CodiTemplate = { id: 0, clothes: [] };
+    Object.entries(codi).forEach((elem) => {
+      if (elem[0] === "id") {
+        theCodi.id = elem[1];
+        return;
+      }
+
+      const cate = categoryMap.get(elem[0]) as I.category;
+      const categoryIdx = template.clothes.findIndex(
+        (codiCloth) => codiCloth.category === cate
+      );
+      if (categoryIdx === -1) return;
+
+      const clothData: I.CodiCloth = {
+        ...template.clothes[categoryIdx],
+        category: cate,
+        image: elem[1],
+      };
+      theCodi.clothes.push(clothData);
+    });
+    codiArray.push(theCodi);
+  });
+
+  return codiArray;
+};
---
+++ src/frontend/src/lib/utils/common.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const deepcopy = (obj: any) => JSON.parse(JSON.stringify(obj));
