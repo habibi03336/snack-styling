7a52cc327f7463235d2798030144558036cc94f2
김명주 <kimmyeongju@gimmyeongjuui-MacBookPro.local>
2022-09-05 13:54:24 +0900

    feat: change header parameter(add token into header)

--- src/backend/spring/src/main/java/com/snackstyling/spring/community/question/controller/QuestionController.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/community/question/controller/QuestionController.java
@@ -1,5 +1,6 @@
 package com.snackstyling.spring.community.question.controller;
 
+import com.snackstyling.spring.common.service.JwtService;
 import com.snackstyling.spring.community.question.dto.QuestionDetailResponse;
 import com.snackstyling.spring.community.question.dto.QuestionRequest;
 import com.snackstyling.spring.community.question.dto.QuestionNumResponse;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin("*")
 public class QuestionController {
     private final QuestionService questionService;
+    private final JwtService jwtService;
     @ApiOperation(value="질문등록",notes = "<strong>질문을 등록한다.</strong>")
     @RequestMapping(value="/api/v1/board/question", method = RequestMethod.POST)
     public ResponseEntity<QuestionNumResponse> postQuestion(@RequestBody QuestionRequest questionRequest) {
@@ -24,13 +26,15 @@ public class QuestionController {
     @ApiOperation(value="질문 목록 불러오기",notes = "<strong>페이지네이션을 통해 부분적으로 질문을 불러온다.</strong>")
     @ApiImplicitParam(name = "page", value = "페이지 번호", required = true, dataType = "int", defaultValue = "None")
     @RequestMapping(value="/api/v1/board/question", method = RequestMethod.GET)
-    public ResponseEntity<QuestionsResponse> loadQuestion(@RequestParam("page") Integer page){
+    public ResponseEntity<QuestionsResponse> loadQuestion(@RequestHeader("Authorization") String token, @RequestParam("page") Integer page){
+        jwtService.validateToken(token);
         return ResponseEntity.ok().body(questionService.questionList(page));
     }
     @ApiOperation(value="질문 상세 내용 보기",notes = "<strong>질문을 클릭하면 상세 내용 및 답변을 볼 수 있다.</strong>")
     @ApiImplicitParam(name = "id", value = "질문 번호", required = true, dataType = "int", defaultValue = "None")
     @RequestMapping(value="api/v1/board/question/{id}", method = RequestMethod.GET)
-    public ResponseEntity<QuestionDetailResponse> detailQuestion(@PathVariable(value="id") Long id) {
+    public ResponseEntity<QuestionDetailResponse> detailQuestion(@RequestHeader("Authorization") String token, @PathVariable(value="id") Long id) {
+        jwtService.validateToken(token);
         return ResponseEntity.ok().body(questionService.questionDetail(id));
     }
     @ApiOperation(value="질문 삭제",notes = "<strong>질문을 삭제한다.</strong>")
