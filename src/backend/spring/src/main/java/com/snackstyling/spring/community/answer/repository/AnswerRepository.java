a6792a05a8465baed86eafd47ef18172ecd44eba
김명주 <kimmyeongju@gimmyeongjuui-MacBookPro.local>
2022-09-05 15:40:56 +0900

    feat: create answer list dto

--- src/backend/spring/src/main/java/com/snackstyling/spring/community/answer/repository/AnswerRepository.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/community/answer/repository/AnswerRepository.java
@@ -2,6 +2,7 @@ package com.snackstyling.spring.community.answer.repository;
 
 import com.snackstyling.spring.community.answer.domain.Answer;
 import com.snackstyling.spring.community.question.domain.Question;
+import com.snackstyling.spring.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +15,7 @@ public interface AnswerRepository extends JpaRepository<Answer,Long> {
     List<Answer> findByQuestionOrderByAdoptDescPostDateAsc(Question question);
     @Query("SELECT COUNT(*) FROM Answer a WHERE a.question=:question")
     Long countByAnswer(@Param("question") Question question);
+
+    List<Answer> findByMember(Member member);
 
 }
