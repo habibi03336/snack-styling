871c623d49620445070e868a52a0dfc49708ce5a
myeongju-kim <mjoo1106@naver.com>
2022-09-19 14:09:13 +0900

    feat: create delete and update function (answer)

--- src/backend/spring/src/main/java/com/snackstyling/spring/community/answer/controller/AnswerController.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/community/answer/controller/AnswerController.java
@@ -1,5 +1,6 @@
 package com.snackstyling.spring.community.answer.controller;
 
+import com.snackstyling.spring.common.service.JwtService;
 import com.snackstyling.spring.community.answer.dto.AnswerNumResponse;
 import com.snackstyling.spring.community.answer.dto.AnswerRequest;
 import com.snackstyling.spring.community.answer.service.AnswerService;
@@ -13,30 +14,28 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin("*")
 public class AnswerController {
     private final AnswerService answerService;
+    private final JwtService jwtService;
     @ApiOperation(value="답변 등록",notes = "<strong>답변 정보를 받아 저장한다.</strong>")
     @RequestMapping(value="/api/v1/board/answer", method = RequestMethod.POST)
     public ResponseEntity<AnswerNumResponse> postAnswer(@RequestHeader("Authorization") String token, @RequestBody AnswerRequest answerRequest){
+        jwtService.validateToken(token);
         return ResponseEntity.ok().body(answerService.postAnswer(token, answerRequest));
     }
-    @ApiOperation(value="답변 상세 조회",notes = "<strong>답변을 상세하게 봅니다.</strong>")
-    @RequestMapping(value="/api/v1/board/answer/{id}", method = RequestMethod.GET)
-    public ResponseEntity detailAnswer(@PathVariable(value="id") String id) {
-        return ResponseEntity.ok().build();
-    }
-    @ApiOperation(value="답변 삭제",notes = "<strong>답변을 삭제합니다.</strong>")
+   @ApiOperation(value="답변 삭제",notes = "<strong>답변을 삭제합니다.</strong>")
     @RequestMapping(value="/api/v1/board/answer/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity deleteAnswer(@PathVariable(value="id") Long id){
+    public ResponseEntity deleteAnswer(@RequestHeader("Authorization") String token, @PathVariable(value="id") Long id){
+        jwtService.validateToken(token);
         answerService.deleteAnswer(id);
         return ResponseEntity.ok().build();
     }
-    /*
+
     @ApiOperation(value="답변 수정",notes = "<strong>답변을 수정합니다.</strong>")
     @RequestMapping(value="/api/v1/board/answer/{id}", method = RequestMethod.PATCH)
-    public ResponseEntity updateAnswer(@PathVariable(value="id") Long id, @RequestBody AnswerRequest answerRequest){
+    public ResponseEntity updateAnswer(@RequestHeader("Authorization") String token, @PathVariable(value="id") Long id, @RequestBody AnswerRequest answerRequest){
+        jwtService.validateToken(token);
         answerService.updateAnswer(id,answerRequest);
         return ResponseEntity.ok().build();
     }
-    */
     @ApiOperation(value="답변 채택",notes = "<strong>답변을 채택합니다.</strong>")
     @RequestMapping(value="/api/v1/board/answer/adopt/{id}", method = RequestMethod.PATCH)
     public ResponseEntity adoptAnswer(@PathVariable(value="id") Long id){
