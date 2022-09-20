4e6389d2a9a09fa729d00a822d37f590e8d7b079
myeongju-kim <mjoo1106@naver.com>
2022-09-20 15:50:44 +0900

    feat: delete unsupported class

--- src/backend/spring/src/main/java/com/snackstyling/spring/login/config/SecurityConfig.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/login/config/SecurityConfig.java
@@ -7,20 +7,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return  http.cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
-                .headers().frameOptions().disable();
+                .headers().frameOptions().disable().and().build();
     }
 }
