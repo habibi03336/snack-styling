d1ca5407571e5055335cc09b83522ceaa13cbb98
최민수 <ms.choe015@gmail.com>
2022-09-20 18:45:46 +0900

    feat: add cloth delete API code

---
+++ src/backend/django/api/cloth/libs.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from io import BytesIO
+from rembg import remove
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+
+def removeBackground(raw_img: InMemoryUploadedFile) -> InMemoryUploadedFile:
+    pil_img = Image.open(raw_img).convert('RGBA')
+
+    max_value = max(pil_img.width, pil_img.height)
+    div_value = max(max_value // 1000, 1)
+    resize_img = pil_img.resize((
+        int(pil_img.width/div_value),
+        int(pil_img.height/div_value),
+    ))
+    new_img = remove(resize_img)
+    print(new_img)
+
+    new_img_io = BytesIO()
+    new_img.save(new_img_io, format='PNG')
+    result = InMemoryUploadedFile(
+        new_img_io, 'ImageField', raw_img.name, 'image/png', new_img_io.getbuffer().nbytes, raw_img.charset
+    )
+    return result
--- src/backend/django/api/cloth/views.py
+++ src/backend/django/api/cloth/views.py
@@ -3,11 +3,11 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-from drf_spectacular.utils import extend_schema, extend_schema_view
-from api.libs import isSelfRequest
+from drf_spectacular.utils import extend_schema_view
+from api.libs import getUserIdFromJWT, isSelfRequest
 
 from api.cloth.paginations import ClothPagePagination
-from api.cloth.serializers import ClothSerializer, ClothDetailSerializer, ClothCreateSerializer, ClothUserCreateSerializer, ClothRetrieveUpdateSerializer, ClothTagSerializer
+from api.cloth.serializers import ClothDestroySerializer, ClothSerializer, ClothDetailSerializer, ClothCreateSerializer, ClothUserCreateSerializer, ClothRetrieveUpdateSerializer, ClothTagSerializer
 from api.permissions import UserAccessPermission
 
 from model.clothmodel.models import Cloth
@@ -39,12 +39,17 @@ class ClothViewSet(ModelViewSet):
         if self.action == 'partial_update':
             return ClothRetrieveUpdateSerializer
         if self.action == 'destroy':
-            return ClothSerializer
+            return ClothDestroySerializer
         if self.action == 'list':
             return ClothDetailSerializer
         if self.action == 'multiple_tag_update':
             return ClothTagSerializer
         return ClothSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        pk = getUserIdFromJWT(request)
+        request.data['id'] = pk
+        return super().update(request, *args, **kwargs)
 
     @action(detail=False, methods=['patch'], url_path="multi-update")
     def multiple_tag_update(self, request, *args, **kwargs):
