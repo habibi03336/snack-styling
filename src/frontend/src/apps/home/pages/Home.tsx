9c30e25108048454cd5b9d959a360235c3b17986
하지훈 <jnh03336@gmail.com>
2022-09-26 17:36:25 +0900

    feat: codiplan (mapping date and codi)

--- src/frontend/src/apps/home/pages/Home.tsx
+++ src/frontend/src/apps/home/pages/Home.tsx
@@ -1,16 +1,45 @@
-import { IonContent, IonDatetime, IonPage } from "@ionic/react";
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonDatetime,
+  IonHeader,
+  IonModal,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 
 import useHome from "../hooks/useHome";
 import WeatherBox from "../components/WeatherBox";
 import RowFiller from "../../common/components/RowFiller";
-import Button from "../../common/components/Button";
-import useCodis from "../../common/hooks/useCodis";
 import CodiCard from "../../closet/components/CodiCard";
+import { useRef } from "react";
+import useCodis from "../hooks/useCodis";
+import CardLayout from "../../closet/components/CardLayout";
+import { POST_CODIPLAN } from "../../../lib/api/codiplan";
 
 const Home = () => {
-  const { setDate, date } = useHome();
+  const { setDate, date, codiSelected, setCodiplan, codiplan } = useHome();
+  const modal = useRef<HTMLIonModalElement>(null);
   const { codis } = useCodis();
+  function dismiss() {
+    modal.current?.dismiss();
+  }
 
+  const codiplanning = async (codiId: number, date: string) => {
+    try {
+      console.log(date);
+      await POST_CODIPLAN(codiId, date);
+      setCodiplan({
+        ...codiplan,
+        [date.split("-")[2]]: codiId,
+      });
+    } catch {
+      console.log("error");
+    }
+    dismiss();
+  };
   return (
     <IonPage>
       <IonContent>
@@ -26,10 +55,14 @@ const Home = () => {
             style={{
               width: `${window.innerWidth - 40}px`,
             }}
-            value={date}
+            mode="ios"
+            value={date.toISOString().split("T")[0]}
             color="primary"
             presentation="date"
-            onIonChange={(e) => setDate(e.detail.value!)}
+            onIonChange={(e) => {
+              const selectedDate = new Date(e.detail.value!.split("T")[0]);
+              setDate(selectedDate);
+            }}
           ></IonDatetime>
           <RowFiller px={10} />
           <WeatherBox />
@@ -49,29 +82,55 @@ const Home = () => {
                   left: "20px",
                   top: "20px",
                 }}
-              >
-                {codis[0] && "8월 27일의 코디"}
-              </div>
+              ></div>
             </div>
-            {codis[0] && (
+            {true && <strong>{date.toDateString()}</strong>}
+            {codiSelected && (
               <CodiCard
                 type="big"
-                codi={codis[0]}
+                codi={codiSelected}
                 comment={""}
                 onCodiClick={function (): void {
                   throw new Error("Function not implemented.");
                 }}
               />
             )}
-          </div>
-          <RowFiller px={10} />
-          <Button color="primary">
-            <div>
-              <div style={{ fontSize: "16px", fontWeight: "600" }}>
-                옷 추천을 확인해 보세요
-              </div>
+            <div style={{ display: codiSelected ? "none" : "block" }}>
+              <div>등록된 코디가 없습니다.</div>
+
+              <IonButton id="open-modal" expand="block">
+                코디 등록하기
+              </IonButton>
             </div>
-          </Button>
+          </div>
+
+          <IonModal ref={modal} trigger="open-modal">
+            <IonHeader>
+              <IonToolbar>
+                <IonTitle>코디 옷장</IonTitle>
+                <IonButtons slot="end">
+                  <IonButton onClick={dismiss}>Close</IonButton>
+                </IonButtons>
+              </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+              <CardLayout
+                cardComponents={codis.map((codi) => {
+                  return (
+                    <CodiCard
+                      key={codi.id}
+                      codi={codi}
+                      comment={""}
+                      onCodiClick={() => {
+                        if (!codi.id) return;
+                        codiplanning(codi.id, date.toISOString().split("T")[0]);
+                      }}
+                    />
+                  );
+                })}
+              />
+            </IonContent>
+          </IonModal>
           <RowFiller px={10} />
         </div>
       </IonContent>
