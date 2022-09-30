4aa7966dca29423b209bbb9aafaa19e4c1e476ac
하지훈 <jnh03336@gmail.com>
2022-09-30 11:03:56 +0000

    feat: update .gitlab-ci.yml

--- src/backend/django/api/permissions.py
+++ src/backend/django/api/permissions.py
@@ -1,36 +1,39 @@
 import requests
+from django.conf import settings
 from rest_framework import permissions, status
 from api.exceptions import AccessTokenExpired, AuthServerConnectionError
 
-from api.libs import decodeJWTPayload, mutableCheck, readEnvValue
+from api.libs import decodeJWTPayload, mutableCheck
+
 
 class UserAccessPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        if readEnvValue('DEBUG') == 'True':
+        if settings.DEBUG == True:
             mutableCheck(request)
             return True
         token = request.META.get('HTTP_AUTHORIZATION')
         url = 'http://backend-spring:8080/api/v1/accounts/token'
         try:
             res = requests.get(url, headers={'Authorization': token})
-            
-            if res.status_code == requests.codes.ok:
-                mutableCheck(request)
-                return True
-            elif res.status_code == status.HTTP_426_UPGRADE_REQUIRED:
-                raise AccessTokenExpired
+
         except requests.exceptions.ConnectionError as e:
             print("ConnectionError:", e)
             raise AuthServerConnectionError
-        
-        
+
+        if res.status_code == requests.codes.ok:
+            mutableCheck(request)
+            return True
+        elif res.status_code == status.HTTP_426_UPGRADE_REQUIRED:
+            raise AccessTokenExpired
+
         return False
-    
+
     def has_object_permission(self, request, view, obj):
         if obj.userId == 1:
             return True
+
         token = request.META.get('HTTP_AUTHORIZATION')
-        
+
         if obj.userId == decodeJWTPayload(token):
             return True
         return False
