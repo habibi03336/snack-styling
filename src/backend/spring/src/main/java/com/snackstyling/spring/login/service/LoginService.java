ef0adec0c639c1d412558c792bae09270bae9f5d
myeongju-kim <mjoo1106@naver.com>
2022-10-10 15:04:28 +0900

    feat: create google social login function

--- src/backend/spring/src/main/java/com/snackstyling/spring/login/service/LoginService.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/login/service/LoginService.java
@@ -3,8 +3,7 @@ package com.snackstyling.spring.login.service;
 import com.snackstyling.spring.common.dto.TokenDto;
 import com.snackstyling.spring.common.service.JwtService;
 import com.snackstyling.spring.login.domain.Login;
-import com.snackstyling.spring.login.dto.AuthRequest;
-import com.snackstyling.spring.login.dto.LoginResponse;
+import com.snackstyling.spring.login.dto.*;
 import com.snackstyling.spring.login.exception.NoneEmailException;
 import com.snackstyling.spring.login.exception.NoneMemberException;
 import com.snackstyling.spring.login.exception.NonePwdException;
@@ -14,17 +13,27 @@ import com.snackstyling.spring.login.repository.LoginRepository;
 import com.snackstyling.spring.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
     private final LoginRepository loginRepository;
+    private final JoinService joinService;
     private final MemberRepository memberRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    @Value("${google.client.id}")
+    private String googleClientId;
+    @Value("${google.client.pw}")
+    private String googleClientPw;
     private final JwtService jwtService;
     public LoginResponse checkUser(AuthRequest authRequest){
         if(!loginRepository.existsByEmail(authRequest.getEmail())){
@@ -47,5 +56,39 @@ public class LoginService {
     public Login selectLogin(Long id){ return loginRepository.findById(id).orElse(null);}
     public Login loginUser(String email){ return loginRepository.findByEmail(email); }
     public Member findMemberId(Login login){return memberRepository.findByLogin(login);}
+    public Object googleLogin(String authCode){
+        RestTemplate restTemplate = new RestTemplate();
+        GoogleRequest googleOAuthRequestParam = GoogleRequest
+                .builder()
+                .clientId(googleClientId)
+                .clientSecret(googleClientPw)
+                .code(authCode)
+                .redirectUri("http://localhost:8080/api/v1/oauth2/google")
+                .grantType("authorization_code").build();
+        ResponseEntity<GoogleResponse> resultEntity = restTemplate.postForEntity("https://oauth2.googleapis.com/token",
+                googleOAuthRequestParam, GoogleResponse.class);
+        String jwtToken=resultEntity.getBody().getId_token();
+        Map<String, String> map=new HashMap<>();
+        map.put("id_token",jwtToken);
+        ResponseEntity<GoogleInfResponse> resultEntity2 = restTemplate.postForEntity("https://oauth2.googleapis.com/tokeninfo",
+                map, GoogleInfResponse.class);
+        String email=resultEntity2.getBody().getEmail();
+        Login user=loginUser(email);
+        if(user!=null){
+            Member member=memberRepository.findByLogin(user);
+            if(member==null){
+                throw new NoneMemberException("맴버정보를 입력하지 않았습니다.");
+            }
+            TokenDto tokens=jwtService.createToken(email, member);
+            return new LoginResponse(tokens);
+        }
+        else{
+            AuthRequest authRequest=new AuthRequest();
+            authRequest.setEmail(email);
+            authRequest.setPwd(resultEntity2.getBody().getAt_hash());
+            return joinService.joinUser(authRequest);
+        }
+    }
+
 
 }
