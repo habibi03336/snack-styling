92624b1a8465f88e2090da9e03d18689734cee2e
myeongju-kim <mjoo1106@naver.com>
2022-10-17 14:45:28 +0900

    fix: modify spring test code

--- src/backend/spring/src/test/java/com/snackstyling/spring/ApplicationTests.java
+++ src/backend/spring/src/test/java/com/snackstyling/spring/ApplicationTests.java
@@ -2,7 +2,9 @@ package com.snackstyling.spring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snackstyling.spring.community.question.dto.QuestionRequest;
+import com.snackstyling.spring.login.domain.Login;
 import com.snackstyling.spring.login.dto.AuthRequest;
+import com.snackstyling.spring.login.dto.LoginResponse;
 import com.snackstyling.spring.member.dto.SuggestionResquest;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultHandler;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -22,38 +28,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@Transactional
 @SpringBootTest
 class ApplicationTests {
 	@Autowired
 	private MockMvc mvc;
 	@Autowired
 	private ObjectMapper objectMapper;
-	@Test
-	@DisplayName("로그인")
+	private String accessToken;
+	private String refreshToken;
+
+	//@BeforeEach
+	@BeforeEach
 	public void testA() throws Exception{
-		/*
-		사용자가 이메일과 패스워드를 입력하여 로그인을 진행한다.
-		성공시 토큰이 발급된다.
-		 */
 		AuthRequest authRequest=new AuthRequest();
 		authRequest.setEmail("test@naver.com");
 		authRequest.setPwd("test9700!");
-		mvc.perform(post("/api/v1/accounts")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(authRequest)))
+		MvcResult mvcResult= mvc.perform(post("/api/v1/accounts")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(authRequest)))
 				.andExpect(status().isOk())
-				.andDo(print());
+				.andReturn();
+		String[] result=mvcResult.getResponse().getContentAsString().split("\"");
+		accessToken=result[9];
+		refreshToken=result[5];
 	}
 	@Test
 	@DisplayName("전체 게시글 조회")
 	public void testB() throws Exception{
-		/*
-		사용자가 전체 게시글을 조회한다.
-		 */
+      /*
+      사용자가 전체 게시글을 조회한다.
+       */
 		mvc.perform(get("/api/v1/board/question")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4")
+						.header("Authorization", accessToken)
 						.param("page","1"))
 				.andExpect(status().isOk())
 				.andDo(print());
@@ -61,13 +67,12 @@ class ApplicationTests {
 	@Test
 	@DisplayName("데이트 게시글 조회")
 	public void testC() throws Exception{
-		/*
-		사용자가 필터링 기능을 통해 데이트에 해당하는 게시글을
-		조회하고자 한다.
-		 */
+      /*
+      사용자가 필터링 기능을 통해 데이트에 해당하는 게시글을
+      조회하고자 한다.
+       */
 		mvc.perform(get("/api/v1/board/question")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4")
+						.header("Authorization", accessToken)
 						.param("page","1")
 						.param("tpo","2"))
 				.andExpect(status().isOk())
@@ -76,13 +81,12 @@ class ApplicationTests {
 	@Test
 	@DisplayName("데이트 게시글, 채택 된 것 조회")
 	public void testD() throws Exception{
-		/*
-		사용자가 필터링 기능을 통해 데이트에 해당하면서
-		채택 된 게시글만 조회하고자 한다.
-		 */
+      /*
+      사용자가 필터링 기능을 통해 데이트에 해당하면서
+      채택 된 게시글만 조회하고자 한다.
+       */
 		mvc.perform(get("/api/v1/board/question")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4")
+						.header("Authorization", accessToken)
 						.param("page","1")
 						.param("adopt","1")
 						.param("tpo","2"))
@@ -90,20 +94,20 @@ class ApplicationTests {
 				.andDo(print());
 	}
 	@Test
+	@Transactional
 	@DisplayName("질문등록")
 	public void testE() throws Exception{
-		/*
-		사용자가 데이트에 입을 옷을 추천해달라는
-		게시글을 올린다.
-		 */
+      /*
+      사용자가 데이트에 입을 옷을 추천해달라는
+      게시글을 올린다.
+       */
 		QuestionRequest request=new QuestionRequest();
 		request.setTpo(2);
 		LocalDate localDate=LocalDate.now();
 		request.setEndDate(localDate);
 		request.setComments("Test 코드 실행입니다.");
 		mvc.perform(post("/api/v1/board/question")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4")
+						.header("Authorization", accessToken)
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(objectMapper.writeValueAsString(request)))
 				.andExpect(status().isOk())
@@ -112,41 +116,39 @@ class ApplicationTests {
 	@Test
 	@DisplayName("마이페이지")
 	public void testF() throws Exception{
-		/*
-		사용자가 마이페이지를 클릭했을 때
-		결과를 호출한다.
-		 */
+      /*
+      사용자가 마이페이지를 클릭했을 때
+      결과를 호출한다.
+       */
 		mvc.perform(get("/api/v1/profile")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4"))
+						.header("Authorization", accessToken))
 				.andExpect(status().isOk())
 				.andDo(print());
 	}
 	@Test
 	@DisplayName("마이페이지-질문목록")
 	public void testG() throws Exception{
-		/*
-		사용자가 올렸던 질문 목록들을
-		확인할 수 있다.
-		 */
+      /*
+      사용자가 올렸던 질문 목록들을
+      확인할 수 있다.
+       */
 		mvc.perform(get("/api/v1/profile/questions")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4"))
+						.header("Authorization", accessToken))
 				.andExpect(status().isOk())
 				.andDo(print());
 	}
 	@Test
+	@Transactional
 	@DisplayName("마이페이지-건의사항")
 	public void testH() throws Exception{
-		/*
-		사용자는 우리 플랫폼을 사용하면서
-		피드백을 남길 수 있다.
-		 */
+      /*
+      사용자는 우리 플랫폼을 사용하면서
+      피드백을 남길 수 있다.
+       */
 		SuggestionResquest suggestionResquest=new SuggestionResquest();
-		suggestionResquest.setContents("test메시지 입니다.");
+		suggestionResquest.setContents("test 메시지 입니다.");
 		mvc.perform(post("/api/v1/profile/suggestions")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4")
+						.header("Authorization", accessToken)
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(objectMapper.writeValueAsString(suggestionResquest)))
 				.andExpect(status().isOk())
@@ -155,50 +157,46 @@ class ApplicationTests {
 	@Test
 	@DisplayName("알림")
 	public void testI() throws Exception{
-		/*
-		사용자가 알림페이지를 열었을 때로
-		자신에게 온 알림을 확인할 수 있다.
-		 */
+      /*
+      사용자가 알림페이지를 열었을 때로
+      자신에게 온 알림을 확인할 수 있다.
+       */
 		mvc.perform(get("/api/v1/notifications")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4"))
+						.header("Authorization", accessToken))
 				.andExpect(status().isOk())
 				.andDo(print());
 	}
 	@Test
 	@DisplayName("알림 모두 읽음")
 	public void testJ() throws Exception{
-		/*
-		사용자가 알림페이지에서
-		모두 읽음 처리했을 경우 호출되는 api이다.
-		 */
+      /*
+      사용자가 알림페이지에서
+      모두 읽음 처리했을 경우 호출되는 api이다.
+       */
 		mvc.perform(patch("/api/v1/notifications")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4"))
+						.header("Authorization", accessToken))
 				.andExpect(status().isOk())
 				.andDo(print());
 	}
 	@Test
 	@DisplayName("Access Token 유효성 검사")
 	public void testK() throws Exception{
-		/*
-		Access Token 의 유효성을 검사할 수 있다.
-		 */
+      /*
+      Access Token 의 유효성을 검사할 수 있다.
+       */
 		mvc.perform(get("/api/v1/accounts/token")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjkwMTc3MDQxLCJpYXQiOjE2NjQyNTcwNDEsIktleSI6Mn0.OnlcPtIz3ciQJu0gzL30yopZgffbalWVrC2YhbgSLO4"))
+						.header("Authorization", accessToken))
 				.andExpect(status().isOk())
 				.andDo(print());
 	}
 	@Test
 	@DisplayName("Access Token 새로 발급")
 	public void testL() throws Exception{
-		/*
-		리프레시 토큰을 비교해보고 유효하다면 새로 발급을 해준다.
-		 */
+      /*
+      리프레시 토큰을 비교해보고 유효하다면 새로 발급을 해준다.
+       */
 		mvc.perform(post("/api/v1/accounts/token")
-						.header("Authorization",
-								"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJFbWFpbCI6InRlc3RAbmF2ZXIuY29tIiwiZXhwIjoxNjY0ODYzNDIxLCJpYXQiOjE2NjQyNTg2MjEsIktleSI6Mn0.sAwMC0WAGIil04UhiR1aQwXDeinrpQDc7WECDV9bqgo"))
+						.header("Authorization", refreshToken))
 				.andExpect(status().isOk())
 				.andDo(print());
 	}
