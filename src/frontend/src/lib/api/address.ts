b80c62404575bbb7b957445c74846a517d582a23
하지훈 <jnh03336@gmail.com>
2022-10-17 18:33:26 +0900

    chore: config service api url

--- src/frontend/src/lib/api/address.ts
+++ src/frontend/src/lib/api/address.ts
@@ -1,8 +1,8 @@
 export default process.env.NODE_ENV === "production"
   ? {
-      mediaAPI: "",
-      imgAPI: "",
-      api: "",
+      mediaAPI: "https://d1564b71azzs0j.cloudfront.net",
+      imgAPI: "http://img.snackstyling.com",
+      api: "http://api.snackstyling.com",
     }
   : {
       mediaAPI: "http://localhost:8000",
