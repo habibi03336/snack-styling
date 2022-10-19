6defbf0c25e04c2e021ff37f439cff452add2d86
하지훈 <jnh03336@gmail.com>
2022-10-19 02:34:40 +0000

    fix: api path

--- src/frontend/src/lib/api/address.ts
+++ src/frontend/src/lib/api/address.ts
@@ -1,8 +1,8 @@
 export default process.env.NODE_ENV === "production"
   ? {
       mediaAPI: "https://d1564b71azzs0j.cloudfront.net",
-      imgAPI: "https://img.snackstyling.com",
-      api: "https://api.snackstyling.com",
+      imgAPI: "https://img.snackstyling.com/api/v1",
+      api: "https://api.snackstyling.com/api/v1",
     }
   : {
       mediaAPI: "http://localhost:8000",
