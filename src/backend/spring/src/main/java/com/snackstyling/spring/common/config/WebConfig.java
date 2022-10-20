1ee82226a72452c5551ef2c1237f46bb045fee10
myeongju-kim <mjoo1106@naver.com>
2022-10-20 16:49:20 +0900

    fix: modify cors options

--- src/backend/spring/src/main/java/com/snackstyling/spring/common/config/WebConfig.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/common/config/WebConfig.java
@@ -6,19 +6,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    /*
+
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,PATCH";
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("https://www.snackstyling.com")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
-
+        /*
         addMapping : 해당 설정을 적요할 API 범위 선택 =-> /** 전체 적용
         allowedOrigins: Origin을 허용할 범위 선택 (생략 시 *와 같은 의미로 전체 허용됨)
         allowedMethods: 허용할 HTTP 메서드 선택
         exposedHeader: 서버에서 반환할 헤더 지정
-
+        */
     }
-    */
+
 }
