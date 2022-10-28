043798ac2188677164921bcc10565e155bb13827
최민수 <ms.choe015@gmail.com>
2022-10-28 19:05:45 +0900

    feat: fix django setting file

--- src/backend/django/coffProject/settings/local.py
+++ src/backend/django/coffProject/settings/local.py
@@ -5,6 +5,20 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PWD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
