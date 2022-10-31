e577d75e70db31b844404e8c77020c7f0c6dfe2f
myeongju-kim <mjoo1106@naver.com>
2022-10-31 17:00:32 +0900

    fix: modify notification api address

--- src/backend/spring/src/main/java/com/snackstyling/spring/common/controller/NotificationController.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/common/controller/NotificationController.java
@@ -15,7 +15,6 @@ public class NotificationController {
     private final NotificationService notificationService;
     private final JwtService jwtService;
     @ApiOperation(value="알람 전송",notes = "<strong>모든 알람을 출력한다.</strong>")
-    @RequestMapping(value="/api/v1/notifications", method = RequestMethod.GET)
     @GetMapping("")
     public ResponseEntity<NotificationsResponse> notifyLoad(@RequestHeader("Authorization") String token){
         jwtService.validateToken(token);
