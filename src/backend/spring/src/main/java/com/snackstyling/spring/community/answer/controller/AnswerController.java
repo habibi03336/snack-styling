efd464b0335fbe715d5960a1b16bf236eae7a42d
myeongju-kim <mjoo1106@naver.com>
2022-10-31 16:31:36 +0900

    refactor: modify answer api address

--- src/backend/spring/src/main/java/com/snackstyling/spring/community/answer/controller/AnswerController.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/community/answer/controller/AnswerController.java
@@ -11,17 +11,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/board/answer")
 public class AnswerController {
     private final AnswerService answerService;
     private final JwtService jwtService;
     @ApiOperation(value="답변 등록",notes = "<strong>답변 정보를 받아 저장한다.</strong>")
-    @RequestMapping(value="/api/v1/board/answer", method = RequestMethod.POST)
+    @PostMapping("")
     public ResponseEntity<AnswerNumResponse> postAnswer(@RequestHeader("Authorization") String token, @RequestBody AnswerRequest answerRequest){
         jwtService.validateToken(token);
         return ResponseEntity.ok().body(answerService.postAnswer(token, answerRequest));
     }
    @ApiOperation(value="답변 삭제",notes = "<strong>답변을 삭제합니다.</strong>")
-    @RequestMapping(value="/api/v1/board/answer/{id}", method = RequestMethod.DELETE)
+   @DeleteMapping("/{id}")
     public ResponseEntity deleteAnswer(@RequestHeader("Authorization") String token, @PathVariable(value="id") Long id){
         jwtService.validateToken(token);
         answerService.deleteAnswer(id);
@@ -29,14 +30,14 @@ public class AnswerController {
     }
 
     @ApiOperation(value="답변 수정",notes = "<strong>답변을 수정합니다.</strong>")
-    @RequestMapping(value="/api/v1/board/answer/{id}", method = RequestMethod.PATCH)
+    @PatchMapping("/{id}")
     public ResponseEntity updateAnswer(@RequestHeader("Authorization") String token, @PathVariable(value="id") Long id, @RequestBody AnswerRequest answerRequest){
         jwtService.validateToken(token);
         answerService.updateAnswer(id,answerRequest,token);
         return ResponseEntity.ok().build();
     }
     @ApiOperation(value="답변 채택",notes = "<strong>답변을 채택합니다.</strong>")
-    @RequestMapping(value="/api/v1/board/answer/adopt/{id}", method = RequestMethod.PATCH)
+    @PatchMapping("/adopt/{id}")
     public ResponseEntity adoptAnswer(@RequestHeader("Authorization") String token, @PathVariable(value="id") Long id){
         jwtService.validateToken(token);
         answerService.adoptAnswer(id, token);
