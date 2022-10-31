9590f5850692eecea24985be069b39089132158c
myeongju-kim <mjoo1106@naver.com>
2022-10-31 14:23:26 +0900

    refactor: modify jwt code and response

--- src/backend/spring/src/main/java/com/snackstyling/spring/common/controller/JwtController.java
+++ src/backend/spring/src/main/java/com/snackstyling/spring/common/controller/JwtController.java
@@ -9,15 +9,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/accounts/token")
 public class JwtController {
     private final JwtService jwtService;
     @ApiOperation(value="Access Token 새로 발급",notes = "<strong>refresh 토큰이 유효하다면 access 새로 발급</strong>")
-    @RequestMapping(value="/api/v1/accounts/token", method = RequestMethod.POST)
+    @PostMapping("")
     public ResponseEntity<AcTokenResponse> createAccessToken(@RequestHeader("Authorization") String token){
         return ResponseEntity.ok().body(jwtService.refreshCompare(token));
     }
     @ApiOperation(value="Access Token 확인",notes = "<strong>accesstoken 토큰이 유효한지 테스트</strong>")
-    @RequestMapping(value="/api/v1/accounts/token", method = RequestMethod.GET)
+    @GetMapping("")
     public ResponseEntity checkAccessToken(@RequestHeader("Authorization") String token){
         jwtService.validateToken(token);
         return ResponseEntity.ok().build();
