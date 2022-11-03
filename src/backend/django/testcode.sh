2917aa6780310d1222b5bddd942b975d532eb7dd
최민수 <ms.choe015@gmail.com>
2022-11-03 13:17:10 +0900

    feat: add comments attr in codi model

--- src/backend/django/testcode.sh
+++ src/backend/django/testcode.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 
 echo "[DB] Running DB update..."
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata data.json
+python manage.py makemigrations --settings=coffProject.settings.local
+python manage.py migrate --settings=coffProject.settings.local
+python manage.py loaddata data.json --settings=coffProject.settings.local
 
 echo "[Test] Running Test code..."
 python manage.py test --settings=coffProject.settings.local
